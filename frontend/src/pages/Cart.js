82e0f1d773d1a2aeff6a991733a4b7303926bb3d
Mola <huylaqn1008@gmail.com>
2023-03-12 18:55:49 +0700

    total price

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -6,17 +6,22 @@ const Cart = () => {
     const productCartItem = useSelector((state) => state.product.cartItem)
     console.log(productCartItem)
 
+    const totalPrice = productCartItem.reduce((acc, curr) => parseFloat(acc.toString().replace(/\./g, '').replace(',', '.')) + parseFloat(curr.total.replace(/\./g, '').replace(',', '.')), 0);
+    console.log(totalPrice.toLocaleString('vi-VN', { minimumFractionDigits: 0 }));
+
+    const totalQuantity = productCartItem.reduce((acc, curr) => acc + parseInt(curr.quanity), 0)
+
     return (
         <div className='p-2 md:p-4'>
-            <h2 className='text-lg md:text-2xl font-bold text-slate-600'>Your Cart Items</h2>
+            <h2 className='text-lg md:text-4xl font-bold text-slate-600'>Your Cart Items</h2>
 
-            <div className=''>
+            <div className='my-5 mx-5 flex gap-4'>
                 {/*display cart items*/}
-                <div className=''>
+                <div className='w-full max-w-5xl' style={{ width: '2800px' }}>
                     {
                         productCartItem.map(e => {
                             return (
-                                <CartProduct 
+                                <CartProduct
                                     key={e._id}
                                     id={e._id}
                                     name={e.name}
@@ -32,7 +37,20 @@ const Cart = () => {
                 </div>
 
                 {/*total cart item*/}
-                <div className=''></div>
+                <div className='w-96 p-4 bg-green-200 rounded-lg shadow-xl ml-auto mx-auto flex flex-col' style={{ height: '400px' }}>
+                    <h2 className='text-3xl font-bold text-blue-600 p-3 border-b-2 border-blue-200'>Order Summary</h2>
+                    <div className='flex flex-col flex-grow-1 w-full p-4'>
+                        <div className='flex items-center mb-2'>
+                            <p className='text-base font-medium text-gray-700'>Total Quantity:</p>
+                            <p className='ml-auto text-lg font-semibold text-gray-900'>{totalQuantity}</p>
+                        </div>
+                        <div className='flex items-center'>
+                            <p className='text-base font-medium text-gray-700'>Total Price:</p>
+                            <p className='ml-auto text-lg font-semibold text-green-800'>{totalPrice}<span className='text-green-600'> VNĐ</span></p>
+                        </div>
+                    </div>
+                    <button className='w-full text-2xl font-bold py-2 text-white bg-red-400 mt-auto'>Payment</button>
+                </div>
             </div>
         </div>
     )
--- frontend/src/redux/productSlice.js
+++ frontend/src/redux/productSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit"
+import { toast } from 'react-hot-toast'
 
 // Khởi tạo state ban đầu cho productReducer
 const initialState = {
@@ -16,18 +17,53 @@ export const productSlice = createSlice({
             state.productList = [...action.payload]
         },
         addCartItem: (state, action) => {
-            console.log(action)
-            const total = action.payload.price
-            state.cartItem = [...state.cartItem, {...action.payload, quanity: 1, total: total}]
+            const check = state.cartItem.some(e => e._id === action.payload._id)
+            if (check) {
+                toast("Already Item In Cart")
+            } else {
+                toast("Item Added Successfully")
+                const total = action.payload.price
+                state.cartItem = [...state.cartItem, { ...action.payload, quanity: 1, total: total }]
+            }
         },
         deleteCartItem: (state, action) => {
+            if (window.confirm('Bạn có muốn xóa sản phẩm này?')) {
+                toast("One Item Delete");
+                const index = state.cartItem.findIndex((e) => e._id === action.payload)
+                state.cartItem.splice(index, 1)
+                console.log(index);
+            }
+        },
+        increaseQuanity: (state, action) => {
+            const index = state.cartItem.findIndex((el) => el._id === action.payload);
+            let quanity = state.cartItem[index].quanity;
+            const qtyInc = ++quanity;
 
-        }
+            state.cartItem[index].quanity = qtyInc;
+
+            const price = Number(state.cartItem[index].price.replace(/\./g, ''));
+            const total = (price * qtyInc).toLocaleString('vi-VN', {minimumFractionDigits: 0}).replace(',', '.')
+
+            state.cartItem[index].total = total;
+        },
+        decreaseQuanity: (state, action) => {
+            const index = state.cartItem.findIndex((el) => el._id === action.payload);
+            let quanity = state.cartItem[index].quanity;
+            if (quanity > 1) {
+                const qtyDec = --quanity;
+                state.cartItem[index].quanity = qtyDec;
+
+                const price = Number(state.cartItem[index].price.replace(/\./g, ''));
+                const total = (price * qtyDec).toLocaleString('vi-VN', {minimumFractionDigits: 0}).replace(',', '.')
+    
+                state.cartItem[index].total = total;
+            }
+        },
     }
 })
 
 // Export ra các hàm action creator để dispatch các action tương ứng với sản phẩm
-export const { setDataProduct, addCartItem, deleteCartItem } = productSlice.actions
+export const { setDataProduct, addCartItem, deleteCartItem, increaseQuanity, decreaseQuanity } = productSlice.actions
 
 // Export reducer đã được xử lý bơi createSlice method của Redux Toolkit
 export default productSlice.reducer
